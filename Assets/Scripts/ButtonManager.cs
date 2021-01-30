f920926c1ca0b202e46468a6d4afddea88b08595
남태희 <skaxogml777@naver.com>
2021-01-30 22:40:28 +0900

    [ADD Add Button
    [ADD Add Button

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void ClickPlay()
+    {
+        Debug.Log("Play!");
+    }
+
+    public void ClickStore()
+    {
+        Debug.Log("Store!");
+    }
+
+    public void ClickSetting()
+    {
+        Debug.Log("Setting!");
+    }
+
+    public void ClickAcade()
+    {
+        Debug.Log("Acade!");
+    }
+}

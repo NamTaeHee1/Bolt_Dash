d254754f606e590ba222b51bca21f18d1b411f31
남태희 <skaxogml777@naver.com>
2021-02-08 22:36:00 +0900

    [ADD] TestScript
    [ADD] TestScript

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    Transform tr;
+    Vector3 Target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tr = GetComponent<Transform>();
+        Target = new Vector3(0, -1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space))
+            tr.position = Vector3.Lerp(tr.position, Target, 0.01f);
     }
 }

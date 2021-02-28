809ff974976cb8de0121f4ca2b64829641c45603
남태희 <skaxogml777@naver.com>
2021-02-28 23:06:40 +0900

    [UPDATE] GameManager
    [UPDATE] GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,7 +18,6 @@ public class GameManager : MonoBehaviour
         LeftThornTransform = LeftThorn.GetComponent<Transform>();
         RightThornTransform = RightThorn.GetComponent<Transform>();
         StartPositionTransform = StartPosition.GetComponent<Transform>();
-        StartCoroutine(WaitAnimation());
     }
 
     // Update is called once per frame
@@ -39,27 +38,7 @@ public class GameManager : MonoBehaviour
 
     public void BeforeTheGameStarts()
     {
-        LeftThorn.SetActive(true);
-        RightThorn.SetActive(true);
-        StartPosition.SetActive(true);
-        LeftThornTransform.position = Vector3.Lerp(LeftThornTransform.position, new Vector3(0, LeftThornTransform.position.y, LeftThornTransform.position.z), ObjectSpeed);
-        RightThornTransform.position = Vector3.Lerp(RightThornTransform.position, new Vector3(0, RightThornTransform.position.y, RightThornTransform.position.z), ObjectSpeed);
-        StartPositionTransform.position = Vector3.Lerp(StartPositionTransform.position, new Vector3(StartPositionTransform.position.x, -4.7f, StartPositionTransform.position.z), ObjectSpeed);
-
-        for(int i = 0; i < gameObjects.Length; i++)
-        {
-            gameObjects[0].GetComponent<Rigidbody2D>().AddForce(Vector2.right * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[1].GetComponent<Rigidbody2D>().AddForce(Vector2.left * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[2].GetComponent<Animator>().SetBool("isGameStart", true);
-            gameObjects[3].GetComponent<Rigidbody2D>().AddForce(Vector2.down * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[4].GetComponent<Rigidbody2D>().AddForce(Vector2.down * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[5].GetComponent<Rigidbody2D>().AddForce(Vector2.down * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[6].GetComponent<Rigidbody2D>().AddForce(Vector2.down * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[7].GetComponent<Rigidbody2D>().AddForce(Vector2.right * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[8].GetComponent<Rigidbody2D>().AddForce(Vector2.right * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[9].GetComponent<Rigidbody2D>().AddForce(Vector2.left * ObjectSpeed, ForceMode2D.Impulse);
-            gameObjects[10].GetComponent<Rigidbody2D>().AddForce(Vector2.left * ObjectSpeed, ForceMode2D.Impulse);
-        }
+        
     }
 
 

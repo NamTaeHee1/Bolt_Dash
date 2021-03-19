9c19fd52c1cddf418aa0bbf0d1ef5a1ce91a2a69
NamTaeHee1 <skaxogml777@naver.com>
2021-03-19 09:09:58 +0900

    [UPDATE] PlayerInputButtonsChangeAnimation
    [UPDATE] PlayerInputButtonsChangeAnimation

--- Assets/Scripts/PlayerInputButtonsChangeAnimation.cs
+++ Assets/Scripts/PlayerInputButtonsChangeAnimation.cs
@@ -25,20 +25,7 @@ public class PlayerInputButtonsChangeAnimation : MonoBehaviour
 
     IEnumerator StartChangeButtonAnimation()
     {
-        for (int i = 0; i < Buttons.Length; i++)
-            if (Buttons[i].activeInHierarchy) CurrentWorkingButton = Buttons[i];
-
-        while(true)
-        {
-            if (isReady)
-                break;
-            RandomNum = Random.Range(0, 3);
-            Debug.Log(RandomNum);
-            Buttons[RandomNum].SetActive(true);
-            CurrentWorkingButton.SetActive(false);
-            CurrentWorkingButton = Buttons[RandomNum];
-            yield return new WaitForSeconds(0.1f);
-        }
+        yield return new WaitForSeconds(1.0f);
     }
 
     

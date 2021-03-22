8054bfc51a50f9c71bc89eae04ea0d4456da1d39
NamTaeHee1 <skaxogml777@naver.com>
2021-03-22 13:31:41 +0900

    [ADD] Dotween
    [ADD] Dotween

--- Assets/Scripts/PlayerInputButtonsChangeAnimation.cs
+++ Assets/Scripts/PlayerInputButtonsChangeAnimation.cs
@@ -7,17 +7,25 @@ public class PlayerInputButtonsChangeAnimation : MonoBehaviour
     [SerializeField] private GameObject[] Buttons;
     [SerializeField] private float MoveSpeed = 3.0f;
     [SerializeField] private List<GameObject> ButtonOrders;
+    Vector3 BasicPosition = new Vector3(-2.65f, -1.22f, 0);
+    bool isStarted = false;
 
-    void Update()
+    void Update() // 1.7 , 4
     {
-        if (Buttons[0].GetComponent<RectTransform>().position.y > 2)
+        if (Input.GetKeyDown(KeyCode.Space))
+            isStarted = true;
+        if(isStarted)
         {
-            Buttons[0].GetComponent<RectTransform>().position = new Vector3(Buttons[0].GetComponent<RectTransform>().position.x,
-            Buttons[0].GetComponent<RectTransform>().position.y - 4, Buttons[0].GetComponent<RectTransform>().position.z);
+            Buttons[0].GetComponent<RectTransform>().anchoredPosition += Vector2.up * MoveSpeed * Time.deltaTime;
+            StartCoroutine(StartChangeButton(5.0f));
         }
-        if (Input.GetKey(KeyCode.Space))
-            Buttons[0].GetComponent<RectTransform>().position += Vector3.up * Time.deltaTime * MoveSpeed;
+        if (Buttons[0].GetComponent<RectTransform>().anchoredPosition.y >= 1.7f)
+            Buttons[0].GetComponent<RectTransform>().anchoredPosition = new Vector2(Buttons[0].GetComponent<RectTransform>().anchoredPosition.x, -4.0f);
     }
 
-    
+    IEnumerator StartChangeButton(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        isStarted = false;
+    }
 }

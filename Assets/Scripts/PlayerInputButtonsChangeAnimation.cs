90b89b5108d03cd7dc96e6627b63110c2f88c73b
NamTaeHee1 <skaxogml777@naver.com>
2021-04-04 21:32:13 +0900

    [UPDATE] PlayerInputButtonsChangeAnimation
    [UPDATE] PlayerInputButtonsChangeAnimation

--- Assets/Scripts/PlayerInputButtonsChangeAnimation.cs
+++ Assets/Scripts/PlayerInputButtonsChangeAnimation.cs
@@ -25,13 +25,16 @@ public class PlayerInputButtonsChangeAnimation : MonoBehaviour
 
     IEnumerator StartChangeButton()
     {
-        Buttons[0].GetComponent<RectTransform>().DOAnchorPosY(1.7f, 1.0f).SetEase(Ease.InBack);
+        for(int i = 0; i < 2; i++)
+            Buttons[i].GetComponent<RectTransform>().DOAnchorPosY(1.7f, 1.0f).SetEase(Ease.InBack);
         yield return new WaitForSeconds(1.0f);
         while (CurrentSpineCount <= SpineCount)
         {
-            Buttons[0].GetComponent<RectTransform>().anchoredPosition = new Vector2(Buttons[0].GetComponent<RectTransform>().anchoredPosition.x, -3.46f);
+            for(int i = 0; i < 2; i++)
+                Buttons[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(Buttons[0].GetComponent<RectTransform>().anchoredPosition.x, -3.46f);
             yield return new WaitForSeconds(0.3f);
-            Buttons[0].GetComponent<RectTransform>().DOAnchorPosY(1.7f, 0.3f);
+            for(int i = 0; i < 2; i++)
+                Buttons[i].GetComponent<RectTransform>().DOAnchorPosY(1.7f, 0.3f);
             CurrentSpineCount++;
         }
     }

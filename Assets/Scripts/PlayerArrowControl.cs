0580142aaa55e2ccba1444033a924b1d4843540a
NamTaeHee1 <skaxogml777@naver.com>
2021-04-08 12:03:08 +0900

    [ADD] PlayerArrowControl
    [ADD] PlayerArrowControl

---
+++ Assets/Scripts/PlayerArrowControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArrowControl : MonoBehaviour
+{
+    Transform ArrowTransform;
+    [SerializeField] private GameObject ArrowObject;
+    [SerializeField] private float ArrowRotateSpeed = 3.0f; 
+
+    void Start() => ArrowTransform = ArrowObject.GetComponent<Transform>();
+
+    private void Update()
+    {
+        ArrowRotate();
+    }
+
+    void ArrowRotate()
+    {
+        if (ArrowTransform.rotation.z >= 0.3f || ArrowTransform.rotation.z <= -0.3f)
+            ArrowRotateSpeed *= -1;
+        ArrowTransform.RotateAround(GetComponentInParent<Transform>().position, Vector3.forward, Time.deltaTime * ArrowRotateSpeed);
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,78 +4,29 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    Transform PlayerTransform;
-    Rigidbody2D PlayerRigidbody;
-    [SerializeField]
-    private float PlayerSpeed = 3.0f;
-    [SerializeField]
-    private float JumpWeight = 30.0f;
-    bool isJump = false;
-    Color GroundBasicColor = new Color(255/255f, 255/255f, 255/255f, 255);
+    private Transform PlayerTransform;
+    private Rigidbody2D PlayerRigid;
+    private Transform ArrowTransform;
 
-    private void Awake()
+    [SerializeField] private float JumpHeight = 5.0f;
+
+    private void Start()
     {
-        PlayerTransform = GetComponent<Transform>();
-        PlayerRigidbody = GetComponent<Rigidbody2D>();
+        PlayerRigid = GetComponent<Rigidbody2D>();
+        ArrowTransform = GameObject.Find("PlayerArrow").GetComponent<Transform>();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            StartCoroutine(UnBeatTime());
-        }
-
         PlayerJump();
     }
 
-    private void FixedUpdate()
+    public void PlayerJump()
     {
-        PlayerMove();
-    }
-
-    private void PlayerJump()
-    {
-        if(Input.GetButton("Jump") && !isJump)
-        {
-            isJump = true;
-            PlayerRigidbody.AddForce(Vector2.up * JumpWeight, ForceMode2D.Impulse);
-        }
-    }
-
-    private void PlayerMove()
-    {
-        float h = Input.GetAxisRaw("Horizontal");
-        PlayerTransform.Translate(new Vector3(h, 0, 0) * Time.deltaTime * PlayerSpeed);
-    }
-
-    IEnumerator UnBeatTime()
-    {
-        int countTime = 0;
-
-        while(countTime < 10)
-        {
-            if (countTime % 2 == 0)
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(0, 255, 105, 90);
-            else
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(0, 255, 105, 180);
-
-            yield return new WaitForSeconds(0.2f);
-
-            countTime++;
-        }
-
-        this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(0, 255, 105, 255);
-
-        yield return null;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-        {
-            isJump = false;
-            collision.gameObject.GetComponent<SpriteRenderer>().color = GroundBasicColor;
-        }
+        if(Input.GetMouseButtonDown(0))
+          {
+            Debug.Log("Click");
+            PlayerRigid.AddForce(ArrowTransform.position * JumpHeight, ForceMode2D.Impulse);
+          }
     }
 }

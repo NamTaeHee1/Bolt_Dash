de5cedea09c4173eb14ba0e101b7377dea8208f4
NamTaeHee1 <skaxogml777@naver.com>
2021-04-12 13:45:03 +0900

    [DELETE] GroundMoveing
    [DELETE] GroundMoveing

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,10 +5,22 @@ using UnityEngine.UI;
 
 public class TestScript : MonoBehaviour
 {
-    Rigidbody2D tr;
-    [SerializeField] Vector2 ForcePosition;
-    Color32 Color;
+    Vector2 MousePosition;
+    Transform tr;
+    Rigidbody2D rigid;
 
-    void Start() => Debug.Log(Color.a);
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
 
+    void Update()
+    {
+        MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float Angle = Mathf.Atan2(MousePosition.y - tr.position.y, MousePosition.x - tr.position.x) * Mathf.Rad2Deg;
+        tr.rotation = Quaternion.AngleAxis(Angle - 90, Vector3.forward);
+        if (Input.GetMouseButtonDown(0))
+            rigid.AddForce(Vector2.up * 3.0f, ForceMode2D.Impulse);
+    }
 }

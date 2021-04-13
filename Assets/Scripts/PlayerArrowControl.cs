f223c827303d2df25d4f7b78faf0ffd36897b540
NamTaeHee1 <skaxogml777@naver.com>
2021-04-13 10:22:53 +0900

    [UPDATE] PlayerArrowControl + GameScene
    [UPDATE] PlayerArrowControl + GameScene

--- Assets/Scripts/PlayerArrowControl.cs
+++ Assets/Scripts/PlayerArrowControl.cs
@@ -10,17 +10,20 @@ public class PlayerArrowControl : MonoBehaviour
 
     void Start() => ArrowTransform = ArrowObject.GetComponent<Transform>();
 
-    private void Update()
-    {
-        ArrowRotate();
-    }
+    void Update() => ArrowRotate();
 
     void ArrowRotate()
     {
         if (ArrowTransform.rotation.z >= 0.3f)
+        {
             ArrowRotateSpeed *= -1;
+            Debug.Log("왼쪽으로");
+        }
         else if (ArrowTransform.rotation.z <= -0.3f)
+        {
             ArrowRotateSpeed *= -1;
+            Debug.Log("오른쪽으로");
+        }
         ArrowTransform.RotateAround(GetComponentInParent<Transform>().position, Vector3.forward, Time.deltaTime * ArrowRotateSpeed);
     }
 }

4badc91014723579fedc5f5af6f642d7a94406cd
NamTaeHee1 <skaxogml777@naver.com>
2021-06-18 21:41:31 +0900

    [UPDATE] PlayerControl
    [UPDATE] PlayerControl

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,27 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+namespace DanielLochner.Assets.SimpleScrollSnap
 {
-    [SerializeField] private Transform PlayerTransform;
-    [SerializeField] private Transform ArrowTransform;
-
-    [SerializeField] private Rigidbody2D PlayerRigid;
-
-    [SerializeField] private GameObject Arrow;
-
-    [SerializeField] private float JumpHeight = 5.0f;
-
-    public void PlayerJump()
+    public class PlayerControl : MonoBehaviour
     {
-        PlayerRigid.AddForce(ArrowTransform.transform.up * JumpHeight, ForceMode2D.Impulse);
+        [SerializeField] private Transform PlayerTransform;
+        [SerializeField] private Transform ArrowTransform;
+
+        [SerializeField] private Rigidbody2D PlayerRigid;
+
+        [SerializeField] private GameObject Arrow;
+
+        [SerializeField] private float JumpHeight = 5.0f;
+
+        public void PlayerJump()
+        {
+            PlayerRigid.AddForce(ArrowTransform.transform.up * JumpHeight, ForceMode2D.Impulse);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.name.Equals("RightWall"))
+                PlayerRigid.AddForce(Vector2.left * 3.0f, ForceMode2D.Impulse);
+            else if (collision.gameObject.name.Equals("LeftWall"))
+                PlayerRigid.AddForce(Vector2.right * 3.0f, ForceMode2D.Impulse);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.name.Equals("RightWall"))
-            PlayerRigid.AddForce(Vector2.left * 3.0f, ForceMode2D.Impulse);
-        else if (collision.gameObject.name.Equals("LeftWall"))
-            PlayerRigid.AddForce(Vector2.right * 3.0f, ForceMode2D.Impulse);
-    }
 }

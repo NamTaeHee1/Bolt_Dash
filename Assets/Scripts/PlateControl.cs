ae0cdf64986bf92c6e4a428176698ffa0c841e49
NamTaeHee1 <skaxogml777@naver.com>
2021-06-19 23:49:05 +0900

    [UPDATE] PlateControl
    [UPDATE] PlateControl

--- Assets/Scripts/PlateControl.cs
+++ Assets/Scripts/PlateControl.cs
@@ -6,22 +6,19 @@ using DG.Tweening;
 public class PlateControl : MonoBehaviour
 {
     Transform PlateTransform;
-    SpriteRenderer PlateSR;
-    Color32 ObjectColor;
+    SpriteRenderer PlateSpriteRenderer;
     bool isStepOn = false;
+
     void Start()
     {
         PlateTransform = GetComponent<Transform>();
-        PlateSR = GetComponent<SpriteRenderer>();
-        ObjectColor = DanielLochner.Assets.SimpleScrollSnap.StoreManager.InGameObjectColor.CircleColor;
-        PlateSR.color = new Color32(ObjectColor.r, ObjectColor.g, ObjectColor.b, 140);
+        PlateSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag.Equals("Player"))
           {
-            PlateSR.color = ObjectColor;
             if(!isStepOn)
                 PlateTransform.DOLocalMoveY(PlateTransform.position.y + 0.05f, 0.3f).SetEase(Ease.InBack, 10);
             isStepOn = true;

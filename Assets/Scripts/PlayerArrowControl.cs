4120d1f77f7edfa7fecf427f2200425c1441e36f
NamTaeHee1 <skaxogml777@naver.com>
2021-06-20 21:30:10 +0900

    [UPDATE] AcadeScene + PlayerArrowControl
    [UPDATE] AcadeScene + PlayerArrowControl

--- Assets/Scripts/PlayerArrowControl.cs
+++ Assets/Scripts/PlayerArrowControl.cs
@@ -15,7 +15,7 @@ public class PlayerArrowControl : MonoBehaviour
 
     void ArrowRotate()
     {
-        if (ArrowTransform.eulerAngles.z >= 60 || ArrowTransform.eulerAngles.z <= 300)
+        if (ArrowTransform.eulerAngles.z >= 60 || ArrowTransform.eulerAngles.z <= -60)
             ArrowRotateSpeed *= -1;
         ArrowTransform.RotateAround(ParentObject.transform.position, Vector3.forward, Time.deltaTime * ArrowRotateSpeed);
     }

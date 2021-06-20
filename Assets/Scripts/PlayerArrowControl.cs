0f428514e64fae035aab95b2a925d57e0ba1d2fc
NamTaeHee1 <skaxogml777@naver.com>
2021-06-20 21:49:42 +0900

    [UPDATE] PlayerArrowControl
    [UPDATE] PlayerArrowControl

--- Assets/Scripts/PlayerArrowControl.cs
+++ Assets/Scripts/PlayerArrowControl.cs
@@ -11,12 +11,5 @@ public class PlayerArrowControl : MonoBehaviour
 
     [SerializeField] private float ArrowRotateSpeed = 3.0f;
 
-    private void Update() => ArrowRotate();
 
-    void ArrowRotate()
-    {
-        if (ArrowTransform.eulerAngles.z >= 60 || ArrowTransform.eulerAngles.z <= -120)
-            ArrowRotateSpeed *= -1;
-        ArrowTransform.RotateAround(ParentObject.transform.position, Vector3.forward, Time.deltaTime * ArrowRotateSpeed);
-    }
 }

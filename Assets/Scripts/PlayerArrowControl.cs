e8e29b1e69fe98e2226915fea76a478648a8bf44
NamTaeHee1 <skaxogml777@naver.com>
2021-06-20 22:02:08 +0900

    [UPDATE] PlayerArrowControl
    [UPDATE] PlayerArrowControl

--- Assets/Scripts/PlayerArrowControl.cs
+++ Assets/Scripts/PlayerArrowControl.cs
@@ -12,6 +12,7 @@ public class PlayerArrowControl : MonoBehaviour
 
     void RotateArrow()
     {
+        Debug.Log(ArrowTransform.eulerAngles.z);
         if (ArrowTransform.eulerAngles.z >= 35 || ArrowTransform.eulerAngles.z <= -100)
             ArrowRotateSpeed *= -1;
         ArrowTransform.Rotate(Vector3.forward * ArrowRotateSpeed * Time.deltaTime);

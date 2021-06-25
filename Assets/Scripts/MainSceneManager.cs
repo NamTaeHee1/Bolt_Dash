2e658d3a2e0f580731ef04c04ab728c4c8ceedbb
NamTaeHee1 <skaxogml777@naver.com>
2021-06-25 20:11:10 +0900

    [UPDATE] MainSceneManager
    [UPDATE] MainSceneManager

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MainSceneManager : MonoBehaviour
 {
     private GameObject AcadeSceneInfo;
+
     public static bool isClickPauseButtonQuit;
 
     private void Awake()

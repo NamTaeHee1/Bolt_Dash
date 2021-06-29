3621b6de1f139bc46f5fc4f9a0b1ea66f9ddd5fc
NamTaeHee1 <skaxogml777@naver.com>
2021-06-29 17:22:16 +0900

    [UPDATE] LoadingManager
    [UPDATE] LoadingManager

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -64,16 +64,19 @@ public class LoadingManager : MonoBehaviour
 
     IEnumerator LoadingTextAnimation()
     {
-        while (true)
+        if (!isSuccessLoadScene)
         {
-            for (int i = 0; i < LoadingText.Length; i++)
+            while (true)
             {
-                LoadingText[i].color = new Color32(255, 255, 255, 255);
-                yield return new WaitForSeconds(0.3f);
+                for (int i = 0; i < LoadingText.Length; i++)
+                {
+                    LoadingText[i].color = new Color32(255, 255, 255, 255);
+                    yield return new WaitForSeconds(0.3f);
+                }
+                for (int i = 0; i < LoadingText.Length; i++)
+                    LoadingText[i].color = new Color32(255, 255, 255, 130);
+                yield return new WaitForSeconds(0.5f);
             }
-            for (int i = 0; i < LoadingText.Length; i++)
-                LoadingText[i].color = new Color32(255, 255, 255, 130);
-            yield return new WaitForSeconds(0.5f);
         }
     }
 

243a6df68ddd1c429ed21834b15738fd791a0f14
NamTaeHee1 <skaxogml777@naver.com>
2021-06-30 20:22:49 +0900

    [UPDATE] ButtonManager
    [UPDATE] ButtonManager

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonManager : MonoBehaviour
+namespace DanielLochner.Assets.SimpleScrollSnap
 {
+    public class ButtonManager : MonoBehaviour
+    {
 
+    }
 }

d6eca8638618ab28d54020907c6ccebc344e2ed8
NamTaeHee1 <skaxogml777@naver.com>
2021-07-04 21:27:00 +0900

    [UPDATE] PlayerArrowControl
    [UPDATE] PlayerArrowControl

--- Assets/Scripts/PlayerArrowControl.cs
+++ Assets/Scripts/PlayerArrowControl.cs
@@ -19,6 +19,32 @@ public class PlayerArrowControl : MonoBehaviour
 
     private void Update()
     {
+        float zValue = ArrowTransform.eulerAngles.z;
+
+        if(zValue == 360)
+        {
+            zValue = 0;
+        }
+        if (zValue > 180)
+        {
+            zValue = 360 - zValue;
+        }
+
+        if (zValue > 45)
+         {
+             float fixedZ = 45f;
+             ArrowRotateSpeed *= -1;
+
+            if(ArrowTransform.eulerAngles.z < 180)
+               {
+               fixedZ = 45f;
+               }
+            else if (ArrowTransform.eulerAngles.z > 180)
+               {
+               fixedZ = -45f;
+               }
+            ArrowTransform.eulerAngles = new Vector3(ArrowTransform.eulerAngles.x, ArrowTransform.eulerAngles.y, fixedZ);
+          }
         ArrowTransform.Rotate(Vector3.forward * ArrowRotateSpeed * Time.deltaTime);
     }
 
@@ -29,8 +55,8 @@ public class PlayerArrowControl : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name.Equals("LeftRotationLimit") || collision.gameObject.name.Equals("RightRotationLimit"))
-            ArrowRotateSpeed *= -1;
+/*        if (collision.gameObject.name.Equals("LeftRotationLimit") || collision.gameObject.name.Equals("RightRotationLimit"))
+            ArrowRotateSpeed *= -1;*/
     }
 
     public float GetJumpPower()

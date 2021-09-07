0ec9464028a0cfbcfae479a3d3414762ebbdec11
NamTaeHee1 <skaxogml777@naver.com>
2021-09-07 22:10:34 +0900

    [UPDATE] CameraViewCanvasControl
    [UPDATE] CameraViewCanvasControl

--- Assets/Scripts/CameraViewCanvasControl.cs
+++ Assets/Scripts/CameraViewCanvasControl.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class CameraViewCanvasControl : MonoBehaviour
 {
+    public CameraViewCanvasControl Instance = null;
+
+    private void Awake()
+    {
+        if (Instance = null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.Instance);
+        }
+        else
+            Destroy(this.gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
